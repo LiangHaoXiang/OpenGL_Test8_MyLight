9d92ec169b6548a52cd5570a673ff72eb2c45c35
lianghaoxiang <1013568397@qq.com>
2021-07-12 23:06:26 +0800

    Initial Commit

---
+++ OpenGL_Test8_MyLight/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test8_MyLight
+//
+//  Created by Haoxiang Liang on 2021/7/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
