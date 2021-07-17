a20a4907c17f2eb6fea300f1e52d2ed29c0654bc
LiangHaoXiang <1013568397@qq.com>
2021-07-17 15:46:15 +0800

    模拟光照射物体，物体材质结构的封装

---
+++ OpenGL_Test8_MyLight/LightShader/LightFragmentShader.cpp
@@ -0,0 +1,37 @@
+#version 330 core
+struct Material {
+    vec3 ambient;       //环境光
+    vec3 diffuse;       //漫反射
+    vec3 specular;      //镜面反射
+    float shininess;    //反光度
+};
+
+uniform Material material;  //材质
+
+out vec4 FragColor;
+
+uniform vec3 lightColor;
+
+in vec3 FragPos;        //片段的坐标位置
+in vec3 Normal;         //片段的法向量
+in vec3 LightPos;       //光源的坐标
+
+void main()
+{
+    //环境光
+    vec3 ambient = lightColor * material.ambient;
+    //漫反射光
+    vec3 norm = normalize(Normal);  //标准化法向量
+    vec3 lightDir = normalize(LightPos - FragPos); //标准化 光与片段位置向量差（光线方向）
+    float diff = max(dot(norm, lightDir), 0.0); //向量点乘 得到cos余弦值
+    vec3 diffuse = lightColor * (diff * material.diffuse);
+    //镜面反射光
+    vec3 viewDir = normalize(-FragPos);    //观察方向
+    vec3 reflectDir = reflect(-lightDir, norm);     //反射光方向
+    float specularStrength = 0.5;   //镜面强度
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);   //pow是次幂函数：x的y次方
+    vec3 specular = lightColor * (spec * material.specular);
+    //各种光的分量相加
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
--- OpenGL_Test8_MyLight/main.cpp
+++ OpenGL_Test8_MyLight/main.cpp
@@ -1,16 +1,316 @@
 //
 //  main.cpp
-//  OpenGL_Test8_MyLight
+//  OpenGL_Test7_Light
 //
-//  Created by Haoxiang Liang on 2021/7/12.
+//  Created by Haoxiang Liang on 2021/7/10.
 //
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "Shader.h"
 #include <iostream>
-using namespace std;
+//引入openGL数学库
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Camera.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, Worlddddaaa!\n";
-    std::cout << "third" << endl;
+using namespace std;
+using namespace glm;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// settings 屏幕宽高
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
+
+float deltaTime = 0.0f; // 当前帧与上一帧的时间差
+float lastFrame = 0.0f; // 上一帧的时间
+bool firstMouse = true;     //是否是第一次获取鼠标输入
+//鼠标在屏幕的初始位置
+float lastX = SCR_WIDTH * 0.5f;
+float lastY = SCR_HEIGHT * 0.5f;
+
+vec3 lightPos = vec3(1.2f, 1.0f, 2.0f);
+//手工添加顶点法向量数据
+float vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+};
+//方块们的各个世界位置
+vec3 cubePositions[] = {
+  vec3( 0.0f,  0.0f,  0.0f),
+  vec3( 2.0f,  5.0f, -15.0f),
+  vec3(-1.5f, -2.2f, -2.5f),
+  vec3(-3.8f, -2.0f, -12.3f),
+  vec3( 2.4f, -0.4f, -3.5f),
+  vec3(-1.7f,  3.0f, -7.5f),
+  vec3( 1.3f, -2.0f, -2.5f),
+  vec3( 1.5f,  2.0f, -2.5f),
+  vec3( 1.5f,  0.2f, -1.5f),
+  vec3(-1.3f,  1.0f, -1.5f)
+};
+
+//Camera camera = Camera();
+Camera camera(vec3(0.0f, 0.0f, 3.0f));
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+    
+    // build and compile our shader program
+    // ------------------------------------
+    char* headDir = "/Users/haoxiangliang/Desktop/代码草稿/OpenGL/OpenGL_Test8_MyLight/OpenGL_Test8_MyLight/";
+    string p1 = string(headDir) + "LightShader/LightVertexShader.cpp";
+    string p2 = string(headDir) + "LightShader/LightFragmentShader.cpp";
+    Shader lightingShader(p1.c_str(), p2.c_str());
+    string p3 = string(headDir) + "LampShader/LampVertexShader.cpp";
+    string p4 = string(headDir) + "LampShader/LampFragmentShader.cpp";
+    Shader lightCubeShader(p3.c_str(), p4.c_str());
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    //被照射的立方体顶点缓冲数组
+    unsigned int cubeVAO;
+    glGenVertexArrays(1, &cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindVertexArray(cubeVAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //光源立方体顶点缓冲数组
+    unsigned int lightCubeVAO;
+    glGenVertexArrays(1, &lightCubeVAO);
+    glBindVertexArray(lightCubeVAO);
+    // we only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need (it's already bound, but we do it again for educational purposes)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    
+    glEnable(GL_DEPTH_TEST);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);//运行时隐藏鼠标光标
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        
+        processInput(window);
+        
+        // render
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //观察矩阵
+        mat4 view = mat4(1);
+        view = camera.GetViewMatrix();
+        //投影矩阵
+        mat4 projection = mat4(1);
+        projection = perspective(radians(camera.Zoom), (float)(SCR_WIDTH / SCR_HEIGHT), 0.1f, 100.0f);
+        
+
+        //模型矩阵
+        mat4 model = mat4(1);
+        for (int i = 0; i < 1; i++) {
+            model = translate(model, cubePositions[i]);
+//            if(i % 3 == 0) {
+//                model = rotate(model, (float)glfwGetTime() * radians(5.0f*i+10.0f), vec3(0.5f, 1.0f, 0.0f));
+//            }
+//            else {
+                model = rotate(model, radians(50.0f), vec3(0.5f, 1.0f, 0.0f));
+//            }
+            
+            lightingShader.use();
+            lightingShader.setVec3("lightPos", lightPos);
+            lightingShader.setVec3("viewPos", camera.Position);
+            lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+            lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+            lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+            lightingShader.setFloat("material.shininess", 256.0f);
+            lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
+            lightingShader.setMat4("model", value_ptr(model));
+            lightingShader.setMat4("view", value_ptr(view));
+            lightingShader.setMat4("projection", value_ptr(projection));
+            glBindVertexArray(cubeVAO);
+            glDrawArrays(GL_TRIANGLES, 0, 36);//绘制被照射立方体
+        }
+        
+        //模型矩阵
+        model = mat4(1.0f);
+        model = translate(model, lightPos);
+        model = scale(model, vec3(0.2f)); // a smaller cube
+        lightCubeShader.use();
+        lightCubeShader.setMat4("model", value_ptr(model));
+        lightCubeShader.setMat4("projection", value_ptr(projection));
+        lightCubeShader.setMat4("view", value_ptr(view));
+        glBindVertexArray(lightCubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);//绘制光源立方体
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightCubeVAO);
+    glDeleteBuffers(1, &VBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
     return 0;
 }
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    switch (key) {
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, true);
+            break;
+        default:
+            break;
+    }
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos){
+//    cout<<"x = "<<xpos<<"  ,  y = "<<ypos<<endl;
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+    
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+
